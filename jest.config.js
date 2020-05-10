013b4443c222c275164a90849e56e1db4b4a078d
EGOIST <0x142857@gmail.com>
2020-05-10 18:33:02 +0800

    test: add simple test

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  testRegex: '(/__test__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/types/'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+}
---
+++ test/index.test.ts
@@ -0,0 +1,39 @@
+import { join } from 'path'
+import execa from 'execa'
+import fs from 'fs-extra'
+
+const cacheDir = join(__dirname, '.cache')
+const bin = join(__dirname, '../dist/cli.js')
+
+beforeAll(async () => {
+  await fs.remove(cacheDir)
+})
+
+function runTest(title: string, files: { [name: string]: string }) {
+  const testDir = join(cacheDir, title)
+  test(title, async () => {
+    await Promise.all(
+      Object.keys(files).map((name) => {
+        return fs.outputFile(join(testDir, name), files[name], 'utf8')
+      })
+    )
+    const { exitCode } = await execa(bin, ['input.js'], {
+      cwd: testDir,
+    })
+    expect(exitCode).toBe(0)
+    const output = await fs.readFile(join(testDir, 'dist/input.js'), 'utf8')
+    expect(output).toMatchInlineSnapshot(`
+      "'use strict';
+
+      var foo2 = \\"foo\\";
+
+      module.exports = foo2;
+      "
+    `)
+  })
+}
+
+runTest('simple', {
+  'input.js': `import foo from './foo';export default foo`,
+  'foo.js': `export default 'foo'`,
+})
