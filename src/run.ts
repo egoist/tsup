8d42caa9eea42b18e412d27292c9481783c8e888
EGOIST <0x142857@gmail.com>
2020-05-14 03:15:07 +0800

    fix: remove console logs

--- src/run.ts
+++ src/run.ts
@@ -3,7 +3,6 @@ import {spawn} from 'child_process'
 export function runCode(filename: string, {
   args
 }: {args: string[]}) {
-  console.log(filename, args)
   const cmd = spawn('node', [filename, ...args], {
     stdio: 'inherit'
   })
