60542f74e092e4f30ddec0453b735494a8e6d0d4
EGOIST <0x142857@gmail.com>
2020-05-14 02:01:03 +0800

    fix: set exit code

--- src/run.ts
+++ src/run.ts
@@ -3,7 +3,10 @@ import {spawn} from 'child_process'
 export function runCode(filename: string, {
   args
 }: {args: string[]}) {
-  spawn('node', [filename, ...args], {
+  const cmd = spawn('node', [filename, ...args], {
     stdio: 'inherit'
+  })
+  cmd.on('exit', code => {
+    process.exitCode = code || 0
   })
 }
