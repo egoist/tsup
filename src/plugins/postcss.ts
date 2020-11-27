14db5628f16a3b366e1a28bb8764ba33f5ae4b9a
EGOIST <0x142857@gmail.com>
2020-11-27 23:42:52 +0800

    fix: make postcss config optional

--- src/plugins/postcss.ts
+++ src/plugins/postcss.ts
@@ -1,8 +1,7 @@
 import fs from 'fs'
 import path from 'path'
-import {Plugin} from 'esbuild'
+import { Plugin } from 'esbuild'
 import { getPostcss } from '../utils'
-
 
 export const postcssPlugin: Plugin = {
   name: 'postcss',
@@ -10,21 +9,37 @@ export const postcssPlugin: Plugin = {
   setup(build) {
     const configCache = new Map()
 
-    build.onLoad({filter: /\.css$/}, async args => {
-      const loadConfig = await import('postcss-load-config')
-    
+    const getPostcssConfig = async (file: string) => {
+      const loadConfig = require('postcss-load-config')
+
+      if (configCache.has(file)) {
+        return configCache.get(file)
+      }
+
+      try {
+        const result = await loadConfig({}, path.dirname(file))
+        configCache.set(file, result)
+        return result
+      } catch (error) {
+        if (error.message.includes('No PostCSS Config found in')) {
+          const result = { plugins: [], options: {} }
+          return result
+        }
+        throw error
+      }
+    }
+
+    build.onLoad({ filter: /\.css$/ }, async (args) => {
       const contents = await fs.promises.readFile(args.path, 'utf8')
 
       // Load postcss config
-      const {plugins, options} = configCache.get(args.path) || (await loadConfig({}, path.dirname(args.path)))
-
-      configCache.set(args.path, {plugins, options})
+      const { plugins, options } = await getPostcssConfig(args.path)
 
       // Return if no postcss plugins are supplied
       if (!plugins || plugins.length === 0) {
         return {
           contents,
-          loader: 'css'
+          loader: 'css',
         }
       }
 
@@ -32,19 +47,23 @@ export const postcssPlugin: Plugin = {
       const postcss = getPostcss()
       if (!postcss) {
         return {
-          errors: [{
-            text: `postcss is not installed`
-          }]
+          errors: [
+            {
+              text: `postcss is not installed`,
+            },
+          ],
         }
       }
 
       // Tranform CSS
-      const result = await postcss?.default(plugins).process(contents, {...options, from: args.path})
+      const result = await postcss
+        ?.default(plugins)
+        .process(contents, { ...options, from: args.path })
 
       return {
         contents: result.css,
         loader: 'css',
       }
     })
-  }
+  },
 }
