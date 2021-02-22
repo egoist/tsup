588fea572a1db372231643291ba8ba44257bc419
EGOIST <0x142857@gmail.com>
2021-02-23 01:20:29 +0800

    test: refactor to use inline snapshot

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,193 +1,337 @@
-import { join } from 'path'
+import { resolve } from 'path'
 import execa from 'execa'
 import fs from 'fs-extra'
 import glob from 'globby'
 
 jest.setTimeout(60000)
 
-const cacheDir = join(__dirname, '.cache')
-const bin = join(__dirname, '../dist/cli.js')
+const cacheDir = resolve(__dirname, '.cache')
+const bin = resolve(__dirname, '../dist/cli.js')
 
 beforeAll(async () => {
   await fs.remove(cacheDir)
   await execa('yarn', { cwd: __dirname })
 })
 
-function runTest(
-  title: string,
+async function run(
+  testDir: string,
   files: { [name: string]: string },
-  options: { flags?: string[]; snapshot?: boolean } = {}
+  options: {
+    flags?: string[]
+  } = {}
 ) {
-  const testDir = join(cacheDir, title)
-  test(title, async () => {
-    await Promise.all(
-      Object.keys(files).map((name) => {
-        return fs.outputFile(join(testDir, name), files[name], 'utf8')
-      })
-    )
-    const { exitCode, stdout, stderr } = await execa(
-      bin,
-      ['input.ts', ...(options.flags || [])],
-      {
-        cwd: testDir,
-      }
-    )
-    if (exitCode !== 0) {
-      throw new Error(stdout + stderr)
+  testDir = resolve(cacheDir, testDir)
+
+  // Write entry files on disk
+  await Promise.all(
+    Object.keys(files).map((name) => {
+      return fs.outputFile(resolve(testDir, name), files[name], 'utf8')
+    })
+  )
+
+  // Run tsup cli
+  const { exitCode, stdout, stderr } = await execa(
+    bin,
+    ['input.ts', ...(options.flags || [])],
+    {
+      cwd: testDir,
     }
-    if (options.snapshot !== false) {
-      const output = await fs.readFile(join(testDir, 'dist/input.js'), 'utf8')
-      expect(output).toMatchSnapshot('output file')
-      const files = await glob('**/*', {
-        cwd: join(testDir, 'dist'),
-      })
-      expect(files).toMatchSnapshot('output file list')
-    }
+  )
+  if (exitCode !== 0) {
+    throw new Error(stdout + stderr)
+  }
+
+  // Get output
+  const output = await fs.readFile(resolve(testDir, 'dist/input.js'), 'utf8')
+  const outFiles = await glob('**/*', {
+    cwd: resolve(testDir, 'dist'),
   })
+
+  return { output, outFiles }
 }
 
-runTest('simple', {
-  'input.ts': `import foo from './foo';export default foo`,
-  'foo.ts': `export default 'foo'`,
+// https://stackoverflow.com/questions/52788380/get-the-current-test-spec-name-in-jest
+const getTestName = () => expect.getState().currentTestName
+
+test('simple', async () => {
+  const { output, outFiles } = await run(getTestName(), {
+    'input.ts': `import foo from './foo';export default foo`,
+    'foo.ts': `export default 'foo'`,
+  })
+  expect(output).toMatchInlineSnapshot(`
+    "\\"use strict\\";Object.defineProperty(exports, \\"__esModule\\", {value: true});// foo.ts
+    var foo_default = \\"foo\\";
+
+    // input.ts
+    var input_default = foo_default;
+
+
+    exports.default = input_default;
+    "
+  `)
+  expect(outFiles).toMatchInlineSnapshot(`
+    Array [
+      "input.js",
+    ]
+  `)
 })
 
-runTest(
-  'bundle graphql-tools with --dts flag',
-  {
-    'input.ts': `export { makeExecutableSchema } from 'graphql-tools'`,
-  },
-  {
-    snapshot: false,
-    flags: ['--dts'],
-  }
-)
-
-runTest(
-  'bundle vue and type-fest with --dts=bundle flag',
-  {
-    'input.ts': `export * from 'vue'
-    export * as TypeFest from 'type-fest'
-    `,
-  },
-  {
-    snapshot: false,
-    flags: [
-      '--dts',
-      'bundle',
-      // For type-only modules you need to externalize it, `--dts bundle` will ignore this option
-      '--external',
-      'type-fest',
-    ],
-  }
-)
-
-runTest(
-  'bundle graphql-tools with --sourcemap flag',
-  {
-    'input.ts': `export { makeExecutableSchema } from 'graphql-tools'`,
-  },
-  {
-    snapshot: false,
-    flags: ['--sourcemap'],
-  }
-)
-
-runTest(
-  'es5 target',
-  {
-    'input.ts': `
-  export class Foo {
-    hi (): void {
-      let a = () => 'foo'
-
-      console.log(a())
+test('bundle graphql-tools with --dts flag', async () => {
+  await run(
+    getTestName(),
+    {
+      'input.ts': `export { makeExecutableSchema } from 'graphql-tools'`,
+    },
+    {
+      flags: ['--dts'],
     }
-  }
-  `,
-  },
-  {
-    flags: ['--target', 'es5'],
-  }
-)
+  )
+})
 
-runTest(
-  'multiple formats',
-  {
+test('bundle vue and type-fest with --dts=bundle flag', async () => {
+  await run(
+    getTestName(),
+    {
+      'input.ts': `export * from 'vue'
+      export * as TypeFest from 'type-fest'
+      `,
+    },
+    {
+      flags: [
+        '--dts',
+        'bundle',
+        // For type-only modules you need to externalize it, `--dts bundle` will ignore this option
+        '--external',
+        'type-fest',
+      ],
+    }
+  )
+})
+
+test('bundle graphql-tools with --sourcemap flag', async () => {
+  await run(
+    getTestName(),
+    {
+      'input.ts': `export { makeExecutableSchema } from 'graphql-tools'`,
+    },
+    {
+      flags: ['--sourcemap'],
+    }
+  )
+})
+
+test('es5 target', async () => {
+  const { output, outFiles } = await run(
+    getTestName(),
+    {
+      'input.ts': `
+    export class Foo {
+      hi (): void {
+        let a = () => 'foo'
+  
+        console.log(a())
+      }
+    }
+    `,
+    },
+    {
+      flags: ['--target', 'es5'],
+    }
+  )
+  expect(output).toMatchInlineSnapshot(`
+    "\\"use strict\\";Object.defineProperty(exports, \\"__esModule\\", {value: true});// input.ts
+    var Foo = /*@__PURE__*/(function () {
+      function Foo () {}
+
+      Foo.prototype.hi = function hi () {
+        var a = function () { return \\"foo\\"; };
+        console.log(a());
+      };
+
+      return Foo;
+    }());
+
+
+    exports.Foo = Foo;
+    "
+  `)
+  expect(outFiles).toMatchInlineSnapshot(`
+    Array [
+      "input.js",
+    ]
+  `)
+})
+
+test('multiple formats', async () => {
+  const { output, outFiles } = await run(
+    getTestName(),
+    {
+      'input.ts': `
+    export const a = 1
+    `,
+    },
+    {
+      flags: ['--format', 'esm,cjs,iife'],
+    }
+  )
+
+  expect(output).toMatchInlineSnapshot(`
+    "\\"use strict\\";Object.defineProperty(exports, \\"__esModule\\", {value: true});// input.ts
+    var a = 1;
+
+
+    exports.a = a;
+    "
+  `)
+  expect(outFiles).toMatchInlineSnapshot(`
+    Array [
+      "input.global.js",
+      "input.js",
+      "input.mjs",
+    ]
+  `)
+})
+
+test('multiple formats and pkg.type is module', async () => {
+  const { output, outFiles } = await run(
+    getTestName(),
+    {
+      'input.ts': `
+    export const a = 1
+    `,
+      'package.json': JSON.stringify({ type: 'module' }),
+    },
+    {
+      flags: ['--format', 'esm,cjs,iife'],
+    }
+  )
+
+  expect(output).toMatchInlineSnapshot(`
+    "// input.ts
+    var a = 1;
+    export {
+      a
+    };
+    "
+  `)
+  expect(outFiles).toMatchInlineSnapshot(`
+    Array [
+      "input.cjs",
+      "input.global.js",
+      "input.js",
+    ]
+  `)
+})
+
+test('multiple formats with legacy output', async () => {
+  const { output, outFiles } = await run(
+    getTestName(),
+    {
+      'input.ts': `
+    export const a = 1
+    `,
+      'package.json': JSON.stringify({ type: 'module' }),
+    },
+    {
+      flags: ['--format', 'esm,cjs,iife', '--legacy-output'],
+    }
+  )
+
+  expect(output).toMatchInlineSnapshot(`
+    "\\"use strict\\";Object.defineProperty(exports, \\"__esModule\\", {value: true});// input.ts
+    var a = 1;
+
+
+    exports.a = a;
+    "
+  `)
+  expect(outFiles).toMatchInlineSnapshot(`
+    Array [
+      "input.js",
+      "esm/input.js",
+      "iife/input.js",
+    ]
+  `)
+})
+
+test('minify', async () => {
+  const { output, outFiles } = await run(
+    getTestName(),
+    {
+      'input.ts': `
+    export function foo() {
+      return 'foo'
+    }
+    `,
+    },
+    {
+      flags: ['--minify'],
+    }
+  )
+
+  expect(output).toMatchInlineSnapshot(`
+    "\\"use strict\\";Object.defineProperty(exports, \\"__esModule\\", {value: true});function o(){return\\"foo\\"}exports.foo = o;
+    "
+  `)
+  expect(outFiles).toMatchInlineSnapshot(`
+    Array [
+      "input.js",
+    ]
+  `)
+})
+
+test('--env flag', async () => {
+  const { output, outFiles } = await run(
+    getTestName(),
+    {
+      'input.ts': `
+    export const env = process.env.NODE_ENV
+    `,
+    },
+    {
+      flags: ['--env.NODE_ENV', 'production'],
+    }
+  )
+
+  expect(output).toMatchInlineSnapshot(`
+    "\\"use strict\\";Object.defineProperty(exports, \\"__esModule\\", {value: true});// input.ts
+    var env = \\"production\\";
+
+
+    exports.env = env;
+    "
+  `)
+  expect(outFiles).toMatchInlineSnapshot(`
+    Array [
+      "input.js",
+    ]
+  `)
+})
+
+test('import css', async () => {
+  const { output, outFiles } = await run(getTestName(), {
     'input.ts': `
-  export const a = 1
-  `,
-  },
-  {
-    flags: ['--format', 'esm,cjs,iife'],
+    import './foo.css'
+    `,
+    'postcss.config.js': `
+    module.exports = {
+      plugins: [require('postcss-simple-vars')()]
+    }
+    `,
+    'foo.css': `
+  $color: blue;
+  
+  .foo {
+    color: $color;
   }
-)
+    `,
+  })
 
-runTest(
-  'multiple formats and pkg.type is module',
-  {
-    'input.ts': `
-  export const a = 1
-  `,
-    'package.json': JSON.stringify({ type: 'module' }),
-  },
-  {
-    flags: ['--format', 'esm,cjs,iife'],
-  }
-)
-
-runTest(
-  'multiple formats with legacy output',
-  {
-    'input.ts': `
-  export const a = 1
-  `,
-    'package.json': JSON.stringify({ type: 'module' }),
-  },
-  {
-    flags: ['--format', 'esm,cjs,iife', '--legacy-output'],
-  }
-)
-
-runTest(
-  'minify',
-  {
-    'input.ts': `
-  export function foo() {
-    return 'foo'
-  }
-  `,
-  },
-  {
-    flags: ['--minify'],
-  }
-)
-
-runTest(
-  '--env flag',
-  {
-    'input.ts': `
-  export const env = process.env.NODE_ENV
-  `,
-  },
-  {
-    flags: ['--env.NODE_ENV', 'production'],
-  }
-)
-
-runTest('import css', {
-  'input.ts': `
-  import './foo.css'
-  `,
-  'postcss.config.js': `
-  module.exports = {
-    plugins: [require('postcss-simple-vars')()]
-  }
-  `,
-  'foo.css': `
-$color: blue;
-
-.foo {
-  color: $color;
-}
-  `,
+  expect(output).toMatchInlineSnapshot(`"\\"use strict\\";"`)
+  expect(outFiles).toMatchInlineSnapshot(`
+    Array [
+      "input.css",
+      "input.js",
+    ]
+  `)
 })
