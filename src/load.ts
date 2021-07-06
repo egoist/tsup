7d03efeb5453b0a30781b2ae0b0735cf4d66abe4
EGOIST <0x142857@gmail.com>
2021-07-06 17:53:41 +0800

    fix: bundle config file
    closes #346

--- src/load.ts
+++ src/load.ts
@@ -2,64 +2,34 @@ import fs from 'fs'
 import { parse as parseJson } from 'jju/lib/parse'
 import JoyCon from 'joycon'
 import path from 'path'
+import { Loader } from 'esbuild'
 import stripJsonComments from 'strip-json-comments'
-import { transform } from 'sucrase'
-
-import { requireFromString } from './require-from-string'
 
 const joycon = new JoyCon()
 
-const configJoycon = new JoyCon({
-  packageKey: 'tsup',
-})
+const loadJson = async (filepath: string) => {
+  try {
+    const content = stripJsonComments(
+      await fs.promises.readFile(filepath, 'utf8')
+    )
+    return parseJson(content)
+  } catch (error) {
+    throw new Error(
+      `Failed to parse ${path.relative(process.cwd(), filepath)}: ${
+        error.message
+      }`
+    )
+  }
+}
 
 const jsonLoader = {
   test: /\.json$/,
   async load(filepath: string) {
-    try {
-      const content = stripJsonComments(
-        await fs.promises.readFile(filepath, 'utf8')
-      )
-      return parseJson(content)
-    } catch (error) {
-      throw new Error(
-        `Failed to parse ${path.relative(process.cwd(), filepath)}: ${
-          error.message
-        }`
-      )
-    }
+    return loadJson(filepath)
   },
 }
 
 joycon.addLoader(jsonLoader)
-configJoycon.addLoader(jsonLoader)
-
-const tsLoader = {
-  test: /\.ts$/,
-  async load(filepath: string) {
-    const content = await fs.promises.readFile(filepath, 'utf8')
-    const { code } = transform(content, {
-      filePath: filepath,
-      transforms: ['imports', 'typescript'],
-    })
-    const mod = requireFromString(code, filepath)
-    return mod.default || mod
-  },
-}
-
-joycon.addLoader(tsLoader)
-configJoycon.addLoader(tsLoader)
-
-const cjsLoader = {
-  test: /\.cjs$/,
-  load(filepath: string) {
-    delete require.cache[filepath]
-    return require(filepath)
-  },
-}
-
-joycon.addLoader(cjsLoader)
-configJoycon.addLoader(cjsLoader)
 
 export function loadTsConfig(cwd: string) {
   return joycon.load(
@@ -70,11 +40,13 @@ export function loadTsConfig(cwd: string) {
 }
 
 export async function loadTsupConfig(cwd: string) {
-  const config = await configJoycon.load(
+  const configJoycon = new JoyCon()
+  const configPath = await configJoycon.resolve(
     [
       'tsup.config.ts',
       'tsup.config.js',
       'tsup.config.cjs',
+      'tsup.config.mjs',
       'tsup.config.json',
       'package.json',
     ],
@@ -82,9 +54,89 @@ export async function loadTsupConfig(cwd: string) {
     path.dirname(cwd)
   )
 
-  if (config.data && config.data.tsup) config.data = config.data.tsup
+  if (configPath) {
+    if (configPath.endsWith('.json')) {
+      let data = await loadJson(configPath)
+      if (configPath.endsWith('package.json')) {
+        data = data.tsup
+      }
+      if (data) {
+        return { path: configPath, data }
+      }
+      return {}
+    }
 
-  return config
+    const config = await bundleConfig(configPath)
+    return { path: configPath, data: config }
+  }
+
+  return {}
+}
+
+function removeFile(filepath: string) {
+  if (fs.existsSync(filepath)) {
+    fs.unlinkSync(filepath)
+  }
+}
+
+async function bundleConfig(configFile: string) {
+  const { build } = await import('esbuild')
+  const outFile = configFile.replace(/\.[a-z]+$/, '.bundled.js')
+  const readConfig = () => {
+    delete eval(`require.cache`)[outFile]
+    const result = require(outFile)
+    removeFile(outFile)
+    return result.default
+  }
+  try {
+    await build({
+      entryPoints: [configFile],
+      format: 'cjs',
+      outfile: outFile,
+      platform: 'node',
+      bundle: true,
+      plugins: [
+        {
+          name: 'ignore',
+          setup(build) {
+            build.onResolve({ filter: /.*/ }, (args) => {
+              if (!path.isAbsolute(args.path) && !/^[\.\/]/.test(args.path)) {
+                return { external: true }
+              }
+            })
+            build.onLoad(
+              { filter: /\.(js|ts|mjs|cjs|jsx|tsx)$/ },
+              async (args) => {
+                const contents = await fs.promises.readFile(args.path, 'utf8')
+                const ext = path.extname(args.path)
+                return {
+                  contents: contents
+                    .replace(
+                      /\b__dirname\b/g,
+                      JSON.stringify(path.dirname(args.path))
+                    )
+                    .replace(/\b__filename\b/g, JSON.stringify(args.path))
+                    .replace(
+                      /\bimport\.meta\.url\b/g,
+                      JSON.stringify(`file://${args.path}`)
+                    ),
+                  loader:
+                    ext === '.mjs' || ext === '.cjs'
+                      ? 'js'
+                      : (ext.slice(1) as Loader),
+                }
+              }
+            )
+          },
+        },
+      ],
+    })
+    const config = readConfig()
+    return config
+  } catch (error) {
+    removeFile(outFile)
+    throw error
+  }
 }
 
 export async function loadPkg(cwd: string) {
