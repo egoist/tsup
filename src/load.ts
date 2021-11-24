3aba009478bd68f1b74c7b55f94f8c9e9ea788f3
EGOIST <0x142857@gmail.com>
2021-11-24 17:41:04 +0800

    fix: use `bundle-require` to bundle `tsup.config.ts`
    skip-release

--- src/load.ts
+++ src/load.ts
@@ -2,7 +2,7 @@ import fs from 'fs'
 import { parse as parseJson } from 'jju/lib/parse'
 import JoyCon from 'joycon'
 import path from 'path'
-import { Loader } from 'esbuild'
+import { bundleRequire } from 'bundle-require'
 import stripJsonComments from 'strip-json-comments'
 import { defineConfig } from './'
 
@@ -65,77 +65,16 @@ export async function loadTsupConfig(
       return {}
     }
 
-    const config = await bundleConfig(configPath)
-    return { path: configPath, data: config }
+    const config = await bundleRequire({
+      filepath: configPath,
+    })
+    return {
+      path: configPath,
+      data: config.mod.tsup || config.mod.default || config.mod,
+    }
   }
 
   return {}
-}
-
-function removeFile(filepath: string) {
-  if (fs.existsSync(filepath)) {
-    fs.unlinkSync(filepath)
-  }
-}
-
-async function bundleConfig(configFile: string) {
-  const { build } = await import('esbuild')
-  const outFile = configFile.replace(/\.[a-z]+$/, '.bundled.cjs')
-  const readConfig = () => {
-    delete require.cache[outFile]
-    const result = require(outFile)
-    removeFile(outFile)
-    return result.tsup || result.default || result
-  }
-  try {
-    await build({
-      entryPoints: [configFile],
-      format: 'cjs',
-      outfile: outFile,
-      platform: 'node',
-      bundle: true,
-      plugins: [
-        {
-          name: 'ignore',
-          setup(build) {
-            build.onResolve({ filter: /.*/ }, (args) => {
-              if (!path.isAbsolute(args.path) && !/^[\.\/]/.test(args.path)) {
-                return { external: true }
-              }
-            })
-            build.onLoad(
-              { filter: /\.(js|ts|mjs|cjs|jsx|tsx)$/ },
-              async (args) => {
-                const contents = await fs.promises.readFile(args.path, 'utf8')
-                const ext = path.extname(args.path)
-                return {
-                  contents: contents
-                    .replace(
-                      /\b__dirname\b/g,
-                      JSON.stringify(path.dirname(args.path))
-                    )
-                    .replace(/\b__filename\b/g, JSON.stringify(args.path))
-                    .replace(
-                      /\bimport\.meta\.url\b/g,
-                      JSON.stringify(`file://${args.path}`)
-                    ),
-                  loader:
-                    ext === '.mjs' || ext === '.cjs'
-                      ? 'js'
-                      : (ext.slice(1) as Loader),
-                }
-              }
-            )
-          },
-        },
-      ],
-    })
-    const config = readConfig()
-    return config
-  } catch (error) {
-    removeFile(outFile)
-    throw error
-  }
 }
 
 export async function loadPkg(cwd: string) {
