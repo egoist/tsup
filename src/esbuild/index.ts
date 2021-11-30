b392c69959e9a98ec2b2479f0f58c246addf9c4f
木杉 <zhmushan@qq.com>
2021-11-30 15:50:26 +0800

    fix: esm shim not working on browser

--- src/esbuild/index.ts
+++ src/esbuild/index.ts
@@ -172,7 +172,7 @@ export async function runEsbuild(
       },
       inject: [
         format === 'cjs' ? path.join(__dirname, '../assets/cjs_shims.js') : '',
-        format === 'esm' ? path.join(__dirname, '../assets/esm_shims.js') : '',
+        format === 'esm' && platform === 'node' ? path.join(__dirname, '../assets/esm_shims.js') : '',
         ...(options.inject || []),
       ].filter(Boolean),
       outdir:
