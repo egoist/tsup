ae30b6814978ec6533f81bd2d0beb88e76864be1
EGOIST <0x142857@gmail.com>
2021-12-07 16:02:10 +0800

    fix(types): build types for `src/index.ts` only
    otherwise `Options` will not be exported by `tsup`, not sure how this happens, probbably a bug in rollup-plugin-dts

--- tsup.config.ts
+++ tsup.config.ts
@@ -5,6 +5,9 @@ export default defineConfig({
   name: 'tsup',
   dts: {
     resolve: true,
+    // build types for `src/index.ts` only
+    // otherwise `Options` will not be exported by `tsup`, not sure how this happens, probbably a bug in rollup-plugin-dts
+    entry: './src/index.ts',
   },
   esbuildPlugins: [
     {
