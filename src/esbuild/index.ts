ec45978b495b0dcd37da885920984f99754953c2
Kamil Kisiela <kamil.kisiela@gmail.com>
2021-12-15 10:48:08 +0100

    fix: Pass noExternal to externalPlugin (#498)
    Co-authored-by: EGOIST <0x142857@gmail.com>

--- src/esbuild/index.ts
+++ src/esbuild/index.ts
@@ -112,6 +112,7 @@ export async function runEsbuild(
     format !== 'iife' &&
       externalPlugin({
         external,
+        noExternal: options.noExternal,
         skipNodeModulesBundle: options.skipNodeModulesBundle,
         tsconfigResolvePaths: options.tsconfigResolvePaths,
       }),
