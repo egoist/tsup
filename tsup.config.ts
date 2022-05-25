6b01221471e5e8a667e4ab4ab33c0a69eda28374
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2022-05-24 22:26:57 -0700

    chore: fix typo (#629)

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   dts: {
     resolve: true,
     // build types for `src/index.ts` only
-    // otherwise `Options` will not be exported by `tsup`, not sure how this happens, probbably a bug in rollup-plugin-dts
+    // otherwise `Options` will not be exported by `tsup`, not sure how this happens, probably a bug in rollup-plugin-dts
     entry: './src/index.ts',
   },
 })
