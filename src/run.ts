9301321b0d1eebcc342932988035e8ac4ba6ca1c
ocavue <ocavue@gmail.com>
2023-11-19 20:28:05 +0800

    Try to break the code format

--- src/run.ts
+++ src/run.ts
@@ -2,9 +2,9 @@ import { spawn } from 'child_process'
 
 export function runCode(filename: string, { args }: { args: string[] }) {
   const cmd = spawn('node', [filename, ...args], {
-    stdio: 'inherit',
+    stdio:      'inherit',
   })
   cmd.on('exit', (code) => {
-    process.exitCode = code || 0
+    process.exitCode = code     || 0
   })
 }
